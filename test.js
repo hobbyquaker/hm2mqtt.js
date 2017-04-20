41a44ab13131aaae208cb057814efacebd30c267
hobbyquaker <hq@ccu.io>
2017-04-20 22:36:59 +0200

    add simple systemtest

--- test.js
+++ test.js
@@ -1,24 +1,29 @@
 #!/usr/bin/env node
 
 const cp = require('child_process');
+const path = require('path');
 
-const cmd = __dirname + '/index.js';
+const cmd = path.join(__dirname, '/index.js');
 const args = ['-m mqtt://127.0.0.1:1883', '--ccu-address', '127.0.0.1', '-v', 'debug'];
 
 const hm2mqtt = cp.spawn(cmd, args);
 
 hm2mqtt.stdout.on('data', (data) => {
+    data = data.toString().replace('\n', '');
     console.log(`stdout: ${data}`);
-});
 
-hm2mqtt.stderr.on('data', (data) => {
     if (data.toString().match(/mqtt connect/)) {
         console.log('\n\ntest passed: successful mqtt connection');
         process.exit(0);
     }
 });
 
-hm2mqtt.on('close', (code) => {
+hm2mqtt.stderr.on('data', data => {
+    data = data.toString().replace('\n', '');
+    console.log(`stderr: ${data}`);
+});
+
+hm2mqtt.on('close', code => {
     console.log(`child process exited with code ${code}`);
 });
 
